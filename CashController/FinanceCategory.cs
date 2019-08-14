23d5c6f2eff0ba8ecb91f6567471469fb753880f
Mateus Gagliardi <mateus.gagliardi@campinas.tds>
2019-08-14 17:20:54 -0300

    Initial commit in CSharp

---
+++ CashController/FinanceCategory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CashController
+{
+    public class FinanceCategory
+    {
+        public FinanceCategory()
+        {
+        }
+
+        private string categoryName;
+        private double realAmount;
+        private double foressenAmount;
+        public bool overloadStatus;
+
+        public void SetCategoryName(string name)
+        {
+            categoryName = name;
+        }
+
+        public string GetCategoryName()
+        {
+            if ( String.IsNullOrEmpty(categoryName) )
+                return "\'categoria sem identificacao\'";
+            else
+                return categoryName;
+        }
+
+        public bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                realAmount += amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if( (realAmount-amount) >= 0)
+            {
+                realAmount -= amount;
+                return true;
+            }
+            else
+            {
+                overloadStatus = true;
+                return false;
+            }
+        }
+
+        public double GetRealAmount()
+        {
+            return realAmount;
+        }
+
+        public double GetForeseenAmount()
+        {
+            return foressenAmount;
+        }
+    }
+}
---
+++ CashController/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CashController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!\n");
+
+            FinanceCategory food = new FinanceCategory();
+
+            food.SetCategoryName("alimentacao");
+            food.Deposit(650.00);
+            if( food.Withdraw(550.00) )
+            {
+                Console.WriteLine("Deposito realizado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Impossivel realizar deposito requisitado");
+                Console.WriteLine("Status do flag de overload {0}", food.overloadStatus);
+            }
+
+            Console.WriteLine($"\nTotal disponivel para a categoria {food.GetCategoryName()}: {food.GetRealAmount()}");
+
+            FinanceCategory[] categories;
+            categories = new FinanceCategory();
+
+            categories[0].SetCategoryName("saude");
+            Console.WriteLine("Nome da primeira categoria: {0}", categories[0].GetCategoryName());
+
+        }
+    }
+}
